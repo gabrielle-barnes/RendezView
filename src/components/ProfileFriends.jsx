f3533e5f8d3a1fae1eceb954433ba69f8724e4a3
umamigabi <123357590+gabrielle-barnes@users.noreply.github.com>
2024-11-13 18:05:01 -0800

    user profile page

---
+++ src/components/ProfileFriends.jsx
@@ -0,0 +1,13 @@
+export default function ProfileFriends() {
+    return (
+        <div>
+            
+            <h2>All Friends</h2>
+            <ul>
+                <li>Friend 1</li>
+                <li>Friend 2</li>
+                <li>Friend 3</li>
+            </ul>
+        </div>
+    );
+}
