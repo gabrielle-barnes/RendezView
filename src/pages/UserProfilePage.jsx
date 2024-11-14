98ed19b25b515caad20b41e9150ab0d501ecd488
KalifByrd <kbyrd1@lion.lmu.edu>
2024-11-13 17:00:46 -0800

    moving calendar to home page

--- src/pages/UserProfilePage.jsx
+++ src/pages/UserProfilePage.jsx
@@ -1,10 +1,7 @@
-import Calendar from "../components/Calendar";
-
 export default function UserProfilePage() {
   return (
     <>
       <h1>This is the user profile page</h1>
-      <Calendar />
     </>
   );
 }
