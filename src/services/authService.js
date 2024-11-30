641ed4d6b4d42333abc3cf7575ad6a26d2905081
umamigabi <123357590+gabrielle-barnes@users.noreply.github.com>
2024-11-29 20:02:47 -0800

    authentication & database :(

--- src/services/authService.js
+++ src/services/authService.js
@@ -1,25 +1,54 @@
-import { useState, useEffect } from "react"
-import { signInWithPopup, GoogleAuthProvider, signOut } from "firebase/auth"
-import { auth } from "../firebaseConfig"
+import { useState, useEffect } from "react";
+import { signInWithPopup, GoogleAuthProvider, signOut } from "firebase/auth";
+import { doc, getDoc, setDoc } from "firebase/firestore";
+import { auth } from "../firebaseConfig";
+import { db } from "../firebaseConfig";
 
-export function login() {
-  return signInWithPopup(auth, new GoogleAuthProvider())
+export async function checkAndCreateUser(user) {
+  const userRef = doc(db, "users", user.uid);
+  const userSnap = await getDoc(userRef);
+
+  if (!userSnap.exists()) {
+    await setDoc(userRef, {
+      bio: "",
+      displayName: user.displayName || "",
+      emails: user.email || "",
+      enemies: [],
+      friendRequests: [],
+      friends: [],
+      enemies: [],
+      profilePhoto: "",
+    });
+  }
+}
+export async function login() {
+  const result = await signInWithPopup(auth, new GoogleAuthProvider());
+  const user = result.user;
+
+  await checkAndCreateUser(user);
+  return user;
 }
 
 export function logout() {
-  return signOut(auth)
+  return signOut(auth);
 }
 
 export function loggedInUserDisplayName() {
-  return auth.currentUser.displayName
+  return auth.currentUser.displayName;
 }
 
 export function useAuthentication() {
-  const [user, setUser] = useState(null)
+  const [user, setUser] = useState(null);
   useEffect(() => {
     return auth.onAuthStateChanged((user) => {
-      user ? setUser(user) : setUser(null)
-    })
-  }, [])
-  return user
+      if (user) {
+        checkAndCreateUser(user).then(() => {
+          setUser(user);
+        });
+      } else {
+        setUser(null);
+      }
+    });
+  }, []);
+  return user;
 }
