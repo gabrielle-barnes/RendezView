22841b61cbabb14c764929a7838262d94b6cc5e8
umamigabi <123357590+gabrielle-barnes@users.noreply.github.com>
2024-11-30 15:50:59 -0800

    succesful met gala API use

--- src/components/ProfileHeader.jsx
+++ src/components/ProfileHeader.jsx
@@ -1,15 +1,27 @@
+import { useState } from "react";
+import fetchRandomArt from "../services/fetchRandomArt";
+import "./ProfileHeader.css";
+
 export default function ProfileHeader() {
+  const [profileImage, setProfileImage] = useState("https://via.placeholder.com/150");
+  const handleFetchRandomArt = async () => {
+    const randomArt = await fetchRandomArt();
+    if (randomArt) setProfileImage(randomArt);
+  };
   return (
     <header className="profile-header">
       <section className="user-info-container">
         <section className="user-identity-container">
           <img
-            src="https://via.placeholder.com/150"
+            src={profileImage}
             alt="Profile"
             className="profile-image"
           />
           <h2 className="profile-name">John Doe</h2>
         </section>
+        <button onClick={handleFetchRandomArt} className="random-art-button">
+          Randomize Picture
+        </button>
         <section className="user-friends-container">
           <p>count</p>
           <p>Friends</p>
