8d9f132179c355a47b8d9d9b86c62268b33fe250
umamigabi <123357590+gabrielle-barnes@users.noreply.github.com>
2024-12-08 14:28:50 -0800

    css additions

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,13 +2,13 @@ import { Link } from "react-router-dom"
 
 export function Navbar() {
   return (
-    <>
+    <div className="navbar-container">
       <Link to="/homepage">
-        <button>Home</button>
+        <button className="home-button">Home</button>
       </Link>
       <Link to="/profile">
-        <button>Your Profile</button>
+        <button className="your-profile-button">Your Profile</button>
       </Link>
-    </>
+    </div>
   )
 }
