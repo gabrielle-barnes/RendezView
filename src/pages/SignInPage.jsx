07fba9e1f88955fb7792f2dd21f05866eb867437
KalifByrd <kbyrd1@lion.lmu.edu>
2024-12-09 14:37:30 -0800

    updated logo and sign in page

--- src/pages/SignInPage.jsx
+++ src/pages/SignInPage.jsx
@@ -1,9 +1,16 @@
 import "../components/SignInPage.css"
+import logo from "../../public/logo.png"
 
 export default function HomePage() {
   return (
-    <>
-      <h2>Please sign in to use the calendar and profile!</h2>
-    </>
+    <div className="home-page">
+      <div className="logo-container">
+        <img src={logo} alt="App Logo" className="floating-logo" />
+      </div>
+      <h2 className="welcome-message">
+        Please sign in to take a <span className="highlight">RendezView</span>{" "}
+        into your plans!
+      </h2>
+    </div>
   )
 }
